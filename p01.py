44ac854e2bf7624ad72f74818a3eaa4da42528b8
fffd06 <dududung039@gmail.com>
2025-09-26 09:51:21 +0900

    4주차

--- p01.py
+++ p01.py
@@ -37,3 +37,14 @@ if age >= 15:
     print("본 영화를 보실 수 있습니다.")
 else :
     print("본 영화를 보실 수 없습니다.")
+
+#6. random 함수
+import random
+
+print("동전 던지기 게임을 시작합니다")
+coin = random.randrange(2)
+if coin == 0:
+    print("앞면입니다")
+else :
+    print("뒷면입니다")
+print("게임이 종료되었습니다")
